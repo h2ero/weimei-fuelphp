b58e9ed80149108db1caf6fc9ff73f287543ce6d
jason <jason@matchmove.com>
2012-11-24 00:35:32 +0800

    init 2

---
+++ fuel/app/app2/classes/controller/welcome.php
@@ -0,0 +1,26 @@
+<?php
+namespace app2;
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ * 
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Welcome extends \app1\Controller_Welcome
+{
+
+	/**
+	 * The basic welcome message
+	 * 
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		echo 2;
+	}
+
+}
