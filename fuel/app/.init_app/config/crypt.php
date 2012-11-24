3afb6ecb262a00271c3ad05e49af345ff03aed23
h2ero <122750707@qq.com>
2012-11-24 09:00:25 +0800

    add app3

---
+++ fuel/app/.init_app/config/crypt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the Fuel framework.
+*
+* @package     Fuel
+* @version     1.0
+* @author      Fuel Development Team
+* @license     MIT License
+* @copyright   2011 Fuel Development Team
+* @link        http://fuelphp.com
+*/
+
+return array (
+		'crypto_key' => 'U-IXYsjkclkcnQwU5UVQoYsw',
+		'crypto_iv' => 's28vq82_8kT8Jw8M9gfwEJHo',
+		'crypto_hmac' => 'd2Msz0qeUohsj8MDhMW80h0o',
+);
