34267283aaff97422591ed7094687d8462cad038
h2ero <122750707@qq.com>
2012-12-01 23:43:24 +0800

    change js order

--- app/app1/config/site.php
+++ app/app1/config/site.php
@@ -7,8 +7,8 @@ return array(
 		'js'=>array(
 				'jquery'=>'jquery-1.7.2.min.js',
 				'less'=>'less-1.3.0.min.js',
+				'main'=>'main.js',
 				'simple style'=>'simpleStyle.js',
-				' main '=>'main.js',
 				'fancy box'=>'jquery.fancybox-1.3.4.js'
 				),
 		'site_name'=>'唯美小站',
