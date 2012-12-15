b7d9197b255ee0ade0a3cb6bbaf9dee00221bab2
h2ero <122750707@qq.com>
2012-12-15 21:59:09 +0800

    fix tag no end of ,

--- app/app1/classes/model/tag.php
+++ app/app1/classes/model/tag.php
@@ -11,7 +11,7 @@ class Model_tag extends \Orm\Model{
 	protected static $_table_name='tag';
 	protected static $_primary_key = array('id');
 	public static function get_tag($target_id){
-		$tag=self::query()->where('target_id','LIKE',"%$target_id%")->get();
+		$tag=self::query()->where('target_id','LIKE',"%$target_id,%")->get();
 		return $tag;
 	}
 
