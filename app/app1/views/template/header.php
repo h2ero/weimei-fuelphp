cb3aaa6ac06c5e749d3bd1937fbcb53663009d3f
h2ero <122750707@qq.com>
2012-12-15 21:44:38 +0800

    change url

--- app/app1/views/template/header.php
+++ app/app1/views/template/header.php
@@ -2,9 +2,9 @@
 		<div id="inner-header">
 			<a id="logo" href="/"><img src="/assets/images/logo.png" id="logo"></a>
 			<div id="menu">
-				<a href="/pic"><img alt="" src="/assets/images/img.png"></a>
-				<a href="/article"><img alt="" src="/assets/images/text.png"></a>
-				<a href="/avatar"><img alt="" src="/assets/images/text.png"></a>
+				<a href="/pic/"><img alt="" src="/assets/images/img.png"></a>
+				<a href="/article/"><img alt="" src="/assets/images/text.png"></a>
+				<a href="/avatar/"><img alt="" src="/assets/images/text.png"></a>
 			</div>
 			<div id="user-msg">你好，请 <a href="/user/login" class="link1">登录</a> 或者 <a href="/user/reg" class="link1">注册</a>，<a href="http://weimei.de/user/lost_pwd" class="link3">忘记密码？</a></div>
 			<div class="fn-clear"></div>
