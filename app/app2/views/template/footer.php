6ae52f2f4ebe1d98827a75ea3b9c34c794e2e2ee
h2ero <122750707@qq.com>
2013-01-03 18:33:27 +0800

    app2  add 51la

---
+++ app/app2/views/template/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+    <div id="copyright">Copyright © 2012 <?php echo $site_name;?></div>
+    <div id="about">
+        <a href="/article/1">关于我们</a>|
+        <a href="#">免责声明</a>|
+        <a href="/article/1">意见反馈</a>|
+        <a href="/user/i/1">联系我们</a>
+    </div>
+    <div class="fn-clear"></div>
+</div>
+<div style="display:none"><script language="javascript" type="text/javascript" src="http://js.users.51.la/3033548.js"></script></div>
+<!--footer-->
