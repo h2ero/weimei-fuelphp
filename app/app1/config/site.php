627454d8eb7e46289d3297104600b446ee1f0592
h2ero <122750707@qq.com>
2013-01-03 18:53:16 +0800

    app1 site domain and seo config change

--- app/app1/config/site.php
+++ app/app1/config/site.php
@@ -18,8 +18,9 @@ return array(
         'upload_css'=>array(
                 'upload'=>'uploadify.css',
                 ),
+        'domain_id'=>'1',
         'site_name'=>'唯美小站',
-        'title'=>'-唯美小站WeiMei.De',
-        'keywords'=>'site config keywords',
-        'description'=>'description'
+        'title'=>'唯美小站WeiMei.De',
+        'keywords'=>'唯美的图片,好看的图片,唯美小站,唯美意境,唯美意境网,唯美图片,小清新图片,唯美的图片,美图,美图网站,每日唯美图片,最新唯美图片,唯美写真,清新美图,唯美图片网,美的网站,欧美唯美图片,唯美头像,唯美壁纸,非主流唯美图片',
+        'description'=>'如果一切美好分为美好和不美好的话，那我希望美好停止于不美好，不美好也停止于美好。'
 );
