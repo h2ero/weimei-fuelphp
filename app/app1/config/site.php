c166c58ddae55ef0f4d51e05c65db1a33b33ae2d
h2ero <122750707@qq.com>
2013-01-12 07:52:55 +0800

    format code

--- app/app1/config/site.php
+++ app/app1/config/site.php
@@ -1,30 +1,31 @@
 <?php
 return array(
-        'css'=>array(
-                'base css'=>'base.css',
-                'fancybox css'=>'fancybox/jquery.fancybox-1.3.4.css',
-                ),
-        'js'=>array(
-                'jquery'=>'jquery-1.7.2.min.js',
-                'less'=>'less-1.3.0.min.js',
-                'fancy box'=>'jquery.fancybox-1.3.4.js',
-                'main'=>'main.js',
-                ),
-        'upload_js'=>array(
-                'swfobject'=>'swfobject.js',
-                'uploadify'=>'jquery.uploadify.min.js',
-                'upload'=>'upload.js',
-                ),
-        'upload_css'=>array(
-                'upload'=>'uploadify.css',
-                ),
-        //theme
-        'theme'=>array(
-                'color'=>'#FF4F73',
-                ),
-        'domain_id'=>'1',
-        'site_name'=>'唯美小站',
-        'title'=>'唯美小站WeiMei.De',
-        'keywords'=>'唯美的图片,好看的图片,唯美小站,唯美意境,唯美意境网,唯美图片,小清新图片,唯美的图片,美图,美图网站,每日唯美图片,最新唯美图片,唯美写真,清新美图,唯美图片网,美的网站,欧美唯美图片,唯美头像,唯美壁纸,非主流唯美图片',
-        'description'=>'如果一切美好分为美好和不美好的话，那我希望美好停止于不美好，不美好也停止于美好。'
+    'css'=>array(
+        'base css'=>'base.css',
+        'fancybox css'=>'fancybox/jquery.fancybox-1.3.4.css',
+        ),
+    'js'=>array(
+        'jquery'=>'jquery-1.7.2.min.js',
+        'less'=>'less-1.3.0.min.js',
+        'fancy box'=>'jquery.fancybox-1.3.4.js',
+        'main'=>'main.js',
+        ),
+    'upload_js'=>array(
+        'swfobject'=>'swfobject.js',
+        'uploadify'=>'jquery.uploadify.min.js',
+        'upload'=>'upload.js',
+        ),
+    'upload_css'=>array(
+        'upload'=>'uploadify.css',
+        ),
+    //theme
+    'theme'=>array(
+        'color'=>'#FF4F73',
+        'logo' => '/assets/images/logo.png',
+        ),
+    'domain_id'=>'1',
+    'site_name'=>'唯美小站',
+    'title'=>'唯美小站WeiMei.De',
+    'keywords'=>'唯美的图片,好看的图片,唯美小站,唯美意境,唯美意境网,唯美图片,小清新图片,唯美的图片,美图,美图网站,每日唯美图片,最新唯美图片,唯美写真,清新美图,唯美图片网,美的网站,欧美唯美图片,唯美头像,唯美壁纸,非主流唯美图片',
+    'description'=>'如果一切美好分为美好和不美好的话，那我希望美好停止于不美好，不美好也停止于美好。'
 );
