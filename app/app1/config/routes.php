b4040a9df61a5f09f274bc351c8986b75e6a565f
h2ero <122750707@qq.com>
2013-01-13 08:21:36 +0800

    add user in routes

--- app/app1/config/routes.php
+++ app/app1/config/routes.php
@@ -6,16 +6,18 @@ return array(
     'avatar/list/(.*)'=>APP_NAME.'/avatar/list/$1',
     'avatar/(.*)'=>APP_NAME.'/avatar/content/$1',
     'avatar'=>APP_NAME.'/avatar/list/1',
-        //pic
+    //pic
     'pic/list/(.*)'=>APP_NAME.'/pic/list/$1',
     'pic/(.*)'=>APP_NAME.'/pic/content/$1',
     'pic'=>APP_NAME.'/pic/list/1',
-        //article
+    //article
     'article/list/(.*)'=>APP_NAME.'/article/list/$1',
     'article/(.*)'=>APP_NAME.'/article/content/$1',
     'article'=>APP_NAME.'/article/list/1',
-        //tag
+    //tag
     'tag/(.*)'=>APP_NAME.'/tag/search/$1',
+    //user
+    'user/(.*)'=>APP_NAME.'/user/$1',
 
     '_root_'  => APP_NAME.'/index',  // The default route
     '_404_'   => APP_NAME.'/error/404',    // The main 404 route
