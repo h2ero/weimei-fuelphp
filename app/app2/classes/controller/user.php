db8726157f8eca10ba4d4d828b2c2194a354be89
h2ero <122750707@qq.com>
2013-01-19 11:31:32 +0800

    add user in app2

---
+++ app/app2/classes/controller/user.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app2;
+
+/**
+ * @name user
+ * @package app1
+ * @author h2ero <122750707@qq.com>
+ * @link http://blog.h2ero.cn
+ */
+class Controller_User extends \app1\Controller_User{
+
+}
